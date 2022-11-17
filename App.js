5e97f9ca73677d5b92d6d658679fdeb50eb0178f
Davis Giang <giangdavis@gmail.com>
2022-11-17 15:08:31 -0800

    Revert "Merge pull request #1 from Erik-Hanson/first-steps"
    This reverts commit e672b6c63277852379b2f17dc7999772abc24b4f, reversing
    changes made to ba1da0bc18bfa534b9270901bdc3b12ad0852728.

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider, Heading, Button } from "native-base";
-
 
 export default function App() {
-  const SiteHeading = () => {
-    return (
-      <Heading size="4xl">STRONGER</Heading>
-    )
-  }
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <SiteHeading></SiteHeading>
-        <StatusBar style="auto" />
-      </View>
-    </NativeBaseProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
@@ -23,7 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
