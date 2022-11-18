291af4c9c77cb8f68ae04ace9380255288b6cb0a
Davis Giang <giangdavis@gmail.com>
2022-11-17 19:47:57 -0800

    Splash Screen Update

--- App.js
+++ App.js
@@ -1,18 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeBaseProvider, Heading, Button } from "native-base";
+import React, { useState, useEffect, useCallback } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SplashScreen from 'expo-splash-screen';
 
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    try {
+      async function prepare() {
+        // Keep the splash screen visible
+        await SplashScreen.preventAutoHideAsync();
+        // Do what you need before the splash screen gets hidden
+        console.log("I'm a task that gets executed before splash screen disappears");
+        setTimeout(async () => {
+          setAppIsReady(true);
+          await SplashScreen.hideAsync();
+        }, 1000);
+      }
+      prepare();
+    } catch(err) {
+      throw(err);
+    }
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // Hide the splash screen
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return <></>;
+  }
+
   const SiteHeading = () => {
     return (
-      <Heading size="4xl">STRONGER</Heading>
+      <Heading size="xl">Home Screen</Heading>
     )
   }
+
+  const StartWorkoutButton = () => {
+    return (
+      <Button size="lg">Home Screen</Button>
+    )
+  }
+
   return (
     <NativeBaseProvider>
       <View style={styles.container}>
-        <SiteHeading></SiteHeading>
+        <SiteHeading style={styles.heading}></SiteHeading>
+        <StartWorkoutButton></StartWorkoutButton>
         <StatusBar style="auto" />
       </View>
     </NativeBaseProvider>
@@ -20,6 +64,9 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  heading: {
+
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
